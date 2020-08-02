ac0251095ff2769674bf38798b2ab0ea303bfd79
Soham Biswas <42530475+sohambiswas4@users.noreply.github.com>
2020-08-02 14:38:40 +0530

    Add files via upload

---
+++ Y--.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium import*
+from selenium.webdriver.common.keys import Keys
+import time
+import pynput
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.request as urllib2
+from pynput.keyboard import Key, Controller
+import pyautogui 
+from keyboard import press		
+
+abc='Pralay Sarkar'
+zz='https://in.yahoo.com/'
+driver = webdriver.Chrome("c:/Users/soham/Downloads/chromedriver_win32/chromedriver")
+
+driver.get(zz)
+time.sleep(0.5)
+search = driver.find_element_by_xpath('//*[@id="header-search-input"]')
+search.send_keys(abc,Keys.ENTER)
+time.sleep(2)
+for a in driver.find_elements_by_xpath('.//a'):
+    print(a.get_attribute('href'))
+    
+    
+#//*[@id="rso"]/div[1]/div/div[1]/a/h3
+#//*[@id="rso"]/div[2]/div/div[1]/a/h3
+#//*[@id="rso"]/div[3]/div/div[1]/a/h3
