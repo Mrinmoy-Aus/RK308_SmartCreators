81444d8278a10cbaf82e48f5000ab1f4c322207e
Mrinmoy Aus <54744863+argho28@users.noreply.github.com>
2020-08-19 19:50:45 +0530

    Update home.py
    twitter updated

--- home.py
+++ home.py
@@ -978,133 +978,6 @@ def facebook_match():
             
     f.close()
 
-def twitter_match():
-    
-    global img_read, img_label
-
-    if(img_label == None):
-        messagebox.showerror("Error", "No image selected. ")
-        return
-
-    crims_found_labels = []
-    for wid in right_frame.winfo_children():
-        wid.destroy()
-
-    frame = cv2.flip(img_read, 1, 0)
-
-    def data(cc):
-        
-        r = requests.get(cc)
-        s = BeautifulSoup(r.text,"html.parser")
-        p = s.find("meta",property ="og:image").attrs['content']
-        with open("a"+".jpg","wb") as pic:
-                binary = requests.get(p).content
-                pic.write(binary)
- 
-
-
-
-
-    (known_face_names, known_face_encodings) = train_model()
-    #unknown_image = face_recognition.load_image_file("img3.jpg")
-    unknown_image = cv2.resize(frame,(500,500))
-    face_locations = detect_faces(frame)
-
-    (frame,name) = recognize_face(frame,known_face_names,known_face_encodings)
-    #print(name)
-           
-            
-    username='mrinmoyaus@gmail.com'
-    password='Mrinmoyaus1998@'
-    abc= ' '.join([str(elem) for elem in name])
-    url='http://www.facebook.com/'
-
-    driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-
-    driver.get(url)
-    driver.find_element_by_id('email').send_keys(username)
-    driver.find_element_by_id('pass').send_keys(password)
-    driver.find_element_by_id('loginbutton').click()
-
-
-    time.sleep(5)
-    search = driver.find_element_by_xpath('//*[@id="mount_0_0"]/div/div/div[1]/div[2]/div[2]/div/div/div[1]/div/div[3]/label/input')
-    search.send_keys(abc,Keys.ENTER)
-
-
-
-
-
-    i=0
-    while(True):
-            try:
-                    action = webdriver.common.action_chains.ActionChains(driver)
-                    a=action.move_to_element_with_offset(search,132,320)
-                    time.sleep(3)
-                    a.click()
-                    action.click()
-                    action.perform()
-                    time.sleep(3)
-            except:
-                    action = webdriver.common.action_chains.ActionChains(driver)
-                    a=action.move_to_element_with_offset(search,132,310)
-                    time.sleep(3)
-                    a.click()
-                    action.click()
-                    action.perform()
-                    time.sleep(3)
-            
-            
-            try:
-                    driver.find_element_by_xpath("//div[@class='rq0escxv l9j0dhe7 du4w35lb j83agx80 cbu4d94t d2edcug0 rj1gh0hx buofh1pr g5gj957u hpfvmrgz dp1hu0rb']//"+"div"+"["+str(i)+"]"+"//div[1]//div[1]//div[1]//div[1]//div[1]//div[2]//div[1]//div[1]//span[1]//div[1]//div[1]//a[1]").click()
-                    print("enter the account")
-                    time.sleep(3)
-                    url = driver.current_url
-                    data(url)
-                    image = face_recognition.load_image_file("a.jpg")
-                    image = cv2.resize(image,(500,500))
-                    (frame,names) = recognize_face(image,known_face_names,known_face_encodings)
-                    named = ' '.join([str(elem) for elem in names])
-                    if named==abc:
-                        copiedText = driver.find_element_by_class_name('sjgh65i0').text
-                        xxx=driver.find_element_by_css_selector('#mount_0_0 > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div > div > div > div.d2edcug0.cbu4d94t.j83agx80.bp9cbjyn > div.rq0escxv.d2edcug0.ecyo15nh.hv4rvrfc.dati1w0a.tr9rh885 > div > div.rq0escxv.l9j0dhe7.du4w35lb.d2edcug0.o387gat7.buofh1pr.g5gj957u.hpfvmrgz.aov4n071.oi9244e8.bi6gxh9e.h676nmdw.aghb5jc5.rek2kq2y > div > div > div > div:nth-child(1) > div').text
-                        print(copiedText)
-                        zz='a'+str(".txt")
-                        file_object  = open(str(zz),'w+')
-                        file_object.write(xxx)
-                        file_object.close()
-                        file_object  = open(str(zz),'a')
-                        file_object.write("\n"+url)
-                        file_object.close()
-                        print("scraping complete")
-                        time.sleep(3)
-                        # driver.execute_script("window.history.go(-1)")
-                        # i+=1
-                        driver.close()
-                        file_object  = open("a.txt",'r')
-                        img_read = cv2.imread("a.jpg")
-                        lab = Label(right_frame,bg="#660033",text=file_object.read(),font="Helvetica 10 bold italic",fg ="yellow")
-                        img_size = 200
-                        showImage(img_read, img_size)
-                        lab.pack()
-                    else:
-                        driver.execute_script("window.history.go(-1)")
-                        i+=1
-            except ElementNotInteractableException:
-                driver.execute_script("window.history.go(-1)")
-                i+=1
-            except NoSuchElementException:
-                driver.execute_script("0, 100*document.body.scrollHeight;")
-                i+=1
-            except AttributeError:
-                driver.execute_script("window.history.go(-1)")
-                i+=1
-            except InvalidSessionIdException:
-                print("Completed")
-
-
-            
-    f.close()
 
 
 def twitter_match():
@@ -1154,10 +1027,10 @@ def twitter_match():
     i=1
     j = 82
     while(True):
-            location = driver.find_element_by_css_selector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div > div > div > div > div > div > section > div > div > div > div:nth-child("+str(i)+") > div > div").location['y']
-            size = driver.find_element_by_css_selector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div > div > div > div > div > div > section > div > div > div > div:nth-child("+str(i)+") > div > div").size['height']
+            location = driver.find_element_by_xpath("//body//div[@class='css-1dbjc4n']//div[@class='css-1dbjc4n']//div["+str(i)+"]//div[1]//div[1]//div[1]//div[2]//div[1]//div[1]//a[1]//div[1]//div[1]//div[1]//span[1]//span[1]").location['y']
+            size = driver.find_element_by_xpath("//body//div[@class='css-1dbjc4n']//div[@class='css-1dbjc4n']//div["+str(i)+"]//div[1]//div[1]//div[1]//div[2]//div[1]//div[1]//a[1]//div[1]//div[1]//div[1]//span[1]//span[1]").size['height']
             #print(location)
-            driver.find_element_by_css_selector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div > div > div > div > div > div > section > div > div > div > div:nth-child("+str(i)+") > div > div").click()
+            driver.find_element_by_xpath("//body//div[@class='css-1dbjc4n']//div[@class='css-1dbjc4n']//div["+str(i)+"]//div[1]//div[1]//div[1]//div[2]//div[1]//div[1]//a[1]//div[1]//div[1]//div[1]//span[1]//span[1]").click()
             time.sleep(1)
             cc = driver.current_url
             #print(cc)
